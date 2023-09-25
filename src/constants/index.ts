fd36f606b5292c6a42c918b9f5baa01aeb776a13
Sooin <sooin0458@gmail.com>
2023-09-25 18:11:56 +0900

    sales plan default

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,6 @@
 export const HOME_PATH = '/';
 
 // salse path
-export const SALES_MENU = '/sales';
 export const SALES_PLAN_PATH = '/sales/sales-plan';
 export const ORDER_INFO_PATH = '/sales/order-info';
 export const RELEASE_INFO_PATH = '/sales/release-info';
