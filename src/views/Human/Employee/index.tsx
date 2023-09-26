385a4c1f8a7c12cce3f260fcb49e903ecf0a55fa
Do0Yeong <kodo7736@naver.com>
2023-09-26 18:13:56 +0900

    Human front ing~

--- src/views/Human/Employee/index.tsx
+++ src/views/Human/Employee/index.tsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import './style.css'
 import HumanMenu from '../HumanMenu'
+import { isVisible } from '@testing-library/user-event/dist/utils'
 
 export default function Employee() {
   return (
     <div>
-      <HumanMenu />
+      <div id='employee-info-wrapper'>
+        <HumanMenu />
+        <div className='employee-info-right'>
+          <div className='employee-info-right-top'>
+            <div className='employee-info-right-top-title'>인사정보등록</div>
+            <div className='employee-info-right-top-divider'></div>
+            <div className='employee-info-right-top-search-condition'>
+              <div className='employee-info-right-top-search-dept'>
+                <div className='employee-info-right-top-search-dept-text'>부서*</div>
+                <div className='employee-info-right-top-search-dept-box'>
+                  <div className='employee-info-right-top-search-dept-box-code'></div>
+                  <div className='employee-info-right-top-search-dept-box-button'>검색</div>
+                  <div className='employee-info-right-top-search-dept-box-name'></div>
+                </div>
+              </div>
+              <div className='employee-info-right-top-search-employee'>
+              <div className='employee-info-right-top-search-employee-text'>사원*</div>
+                <div className='employee-info-right-top-search-employee-box'>
+                  <div className='employee-info-right-top-search-employee-box-code'></div>
+                  <div className='employee-info-right-top-search-employee-box-button'>검색</div>
+                  <div className='employee-info-right-top-search-employee-box-name'></div>
+                </div>              
+              </div>
+              <div className='employee-info-right-top-search-employment-status'>
+                <div className='employee-info-right-top-search-employment-status-text'>재직구분</div>
+                <div className='employee-info-right-top-search-employment-status-combo-box'></div>
+              </div>
+            </div>
+          </div>
+          <div className='employee-info-right-body'>
+            <div className='employee-info-right-body-left'></div>
+            <div className='employee-info-right-body-right'></div>
+          </div>
+          <div className='employee-info-right-bottom'></div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/views/Human/WorkTime/index.tsx
+++ src/views/Human/WorkTime/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './style.css'
+import HumanMenu from '../HumanMenu'
 
 export default function WorkTime() {
   return (
     <div>
+      <HumanMenu />
     </div>
   )
 }
