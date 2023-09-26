9955ee01c8f5298f78fbb64376dbb8461000dace
jhs1145 <jhs1145@gmail.com>
2023-09-26 18:21:31 +0900

    merge

---
+++ src/interfaces/request/auth/sign-in.request.dto.ts
@@ -0,0 +1,4 @@
+export default interface SignInRequestDto {
+     employeeCode: number;
+     password: string;
+}
--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div>
+      사원번호 <input type="text" />
+      비밀번호 <input type="password" />
+    </div>
   )
 }
