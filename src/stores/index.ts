2dd926ca227ab65b1df94ade0b476cb9c23b73f4
Sooin <sooin0458@gmail.com>
2023-10-10 19:05:04 +0900

    sooin front merge

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,12 +2,12 @@ import useUserStore from "./user.store";
 import useCompoanyInfoStore from './companyinfo.store';
 import useInvoiceListStore from './invoicelist.response.store';
 import useInvoiceRequestStore from './invoicelist.request.store';
-import useCustomerInfoStore from './customerinfo.store';
 import useDepartmentInfoStore from './departmentinfo.store';
 import useDepartmentRequsetStore from './departmentlist.request.store';
 import useDepartmentResponseStore from './departmentlist.response.store';
 import useProductListStore from './productlist.request.store';
 import useProductRequestStore from './productlist.request.store';
+import useCustomerInfoStore from './customerinfo.store';
 import useCustomerRequestStore from './customerlist.request.store'
 import useCustomerResponseStore from './customerlist.response.store'
 import useSalesPlanRequestStore from './salesplanlist.request.store'
