a4949dc392318d645f81ed0174d4338f71bb8c11
Do0Yeong <kodo7736@naver.com>
2023-10-26 18:15:07 +0900

    2023-10-26 급상여등록

---
+++ src/interfaces/response/human/get-employee-list.response.dto.ts
@@ -0,0 +1,6 @@
+import ResponseDto from "../response.dto";
+import EmployeeListResponseDto from "./employee-list.response.dto";
+
+export default interface GetEmployeeListResponseDto extends ResponseDto {
+    employeeList: EmployeeListResponseDto[];
+}
